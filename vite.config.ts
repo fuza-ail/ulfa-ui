7f3d7e8b2d3e69e53057b7e474bac11c6020e864
fuzail <mfuzail1991@gmail.com>
2025-09-14 20:03:52 +0700

    config: add storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-
-import { resolve } from "node:path"
+/// <reference types="vitest/config" />
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "UlfaUI",
-      fileName: "ulfa-ui",
+      fileName: "ulfa-ui"
     },
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"],
@@ -19,9 +25,32 @@ export default defineConfig({
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
-          "react/jsx-runtime": "react/jsx-runtime",
-        },
-      },
-    },
+          "react/jsx-runtime": "react/jsx-runtime"
+        }
+      }
+    }
   },
-})
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
+});
